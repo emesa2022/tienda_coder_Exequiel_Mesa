eb0de2423edadfda117d48dd7d13bf97c3880ed2
Exequiel Mesa <micorreo2@gmail.com>
2022-12-11 23:45:08 -0300

    Agrando fetch al index

--- js/main.js
+++ js/main.js
@@ -337,3 +337,16 @@ function actualizarNumerito() {
     numerito.innerText = nuevoNumerito;
 }
 
+const listaPokemon = document.querySelector("#lista-pokemon");
+
+fetch("https://pokeapi.co/api/v2/pokemon")
+    .then(response => response.json())
+    .then(data => {
+        const pokemons = data.results;
+
+        pokemons.forEach(pokemon => {
+            const li = document.createElement("li");
+            li.innerText = pokemon.name;
+            listaPokemon.append(li);
+        });
+    })
